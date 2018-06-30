0433187e91b17e44cfe34af307d9810be13b4cf7
David Braga <dbraga1985@gmail.com>
2018-07-01 00:32:53 +0200

    create User model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require ('mongoose');
+
+//primero definimos un esquema
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email : {type: String,unique:true},
+    password : String
+}); 
+
+
+
+
+//creamos el modelo
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;

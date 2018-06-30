f94f700f6a1b6fd08edade32a41b24f7fa0a0e44
David Braga <dbraga1985@gmail.com>
2018-07-01 00:10:37 +0200

    generate v1/ads route with a provisional http request method to test

---
+++ routes/v1/ads.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const router = express.Router();
+
+const Ad = require('../../models/Ad');
+
+
+router.get("/",(req,res,next)=>{
+    const ad = new Ad({
+        name: 'name1',
+        status: 'sell',
+        precio : 12.5,
+        foto: '../../images/photo1.jpg',
+        tags: ['work','lifestyle']
+    });
+    
+     ad.save(((err, adCreado)=>{
+         if(err){
+             next(err);
+             return;
+         }
+         console.log(adCreado);
+         res.send('ok');
+
+     }));
+});
+
+module.exports = router;

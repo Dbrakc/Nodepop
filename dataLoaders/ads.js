008ac1189556275fc960fbde1bd2e611de0b2906
David Braga <dbraga1985@gmail.com>
2018-07-01 10:29:10 +0200

    create loader of Ad data

---
+++ dataLoaders/ads.js
@@ -0,0 +1,40 @@
+'use strict';
+const Ad = require ('../models/Ad.js');
+const saveArrayData= require('../lib/saveArrayData');
+
+module.exports = {
+    ads : [
+        {
+            name: 'ad1',
+            status: 'sell',
+            price : 12.5,
+            foto: '../../images/photo1.jpg',
+            tags: ['work','lifestyle']
+        },
+        {
+            name: 'ad2',
+            status: 'search',
+            price : 20.3,
+            photo: '../../images/photo2.jpg',
+            tags: ['motor','mobile']
+        },
+        {
+            name: 'ad3',
+            status: 'search',
+            price : 40.9,
+            photo: '../../images/photo3.jpg',
+            tags: ['motor','mobile']
+
+        }
+    ],
+    initCollectionWithData: function (){
+        Ad.remove({},(err)=>{  
+            if(err){
+                console.error(err);
+                return;
+            }
+        });    
+       const adsArray = this.ads.map( ad=>new Ad(ad));
+       saveArrayData(adsArray);
+    }
+};

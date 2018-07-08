95599001f0150a08df45aad086548abc6f8d20ce
David Braga <dbraga1985@gmail.com>
2018-07-08 09:59:49 +0200

    static image downloader

--- dataLoaders/ads.js
+++ dataLoaders/ads.js
@@ -8,21 +8,21 @@ module.exports = {
             name: 'ad1',
             status: 'sell',
             price : 12.5,
-            photo: '../../images/photo1.jpg',
+            photo: 'images/image1.jpg',
             tags: ['work','lifestyle']
         },
         {
             name: 'ad2',
             status: 'search',
             price : 20.3,
-            photo: '../../images/photo2.jpg',
+            photo: 'images/image2.jpg',
             tags: ['motor','mobile']
         },
         {
             name: 'ad3',
             status: 'search',
             price : 40.9,
-            photo: '../../images/photo3.jpg',
+            photo: 'images/image3.jpg',
             tags: ['motor','mobile']
 
         }
